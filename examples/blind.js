e4b591855e49168ac32b55156d0c4e83b5d2f598
Georgios Konstantopoulos <me@gakonst.com>
2020-04-13 13:31:53 +0300

    docs: improve docs on simple example

--- examples/blind.js
+++ examples/blind.js
@@ -1,30 +1,31 @@
-// Choose target depending on https://rustwasm.github.io/docs/wasm-pack/commands/build.html#target
-// We use --target nodejs here
+// Simple Example of blinding, signing, unblinding and verifying.
+
+// Import the library
 const threshold = require("../pkg/threshold")
+const crypto = require('crypto')
 
+// Get a message and a secret for the user
 const msg = Buffer.from("hello world")
-const user_seed = Buffer.from("the user seed which must be at least 32 bytes or else it will return _unreachable_")
+const user_seed = crypto.randomBytes(32)
 
+// Blind the message
 const blinded_msg = threshold.blind(msg, user_seed)
 const blind_msg = blinded_msg.message
 
-// generate the service key
-const service_seed = Buffer.from("servie seed (must be at least 32 bytes or else it will return _unreachable_)")
+// Generate a keypair for the service
+const service_seed = crypto.randomBytes(32)
 const keypair = threshold.keygen(service_seed)
-// bind it locally because otherwise it will clone each time you call the struct
 const private_key = keypair.private
 const public_key = keypair.public
 
-// signature gets signed by service
+// Sign the user's blinded message with the service's private key
 const blind_sig = threshold.sign(private_key, blind_msg)
 
-// user unblinds the signature
+// User unblinds the signature with this scalar
 const unblinded_sig = threshold.unblind_signature(blind_sig, blinded_msg.scalar)
 
-// user verifies the msg by the pubkey of the service
+// User verifies the unblinded signautre on his unblinded message
 const verified = threshold.verify_sign(public_key, msg, unblinded_sig)
-
-// should be true
 if (verified === true) {
     console.log("Verification successful")
 } else {
