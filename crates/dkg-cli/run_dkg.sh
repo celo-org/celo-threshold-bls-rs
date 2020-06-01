09743579a7dffaef32aaece297b4e73f0994c24d
Kobi Gurkan <kobigurk@gmail.com>
2020-06-01 11:55:54 +0300

    feat(dkg): makes command failures more robust and adjusts group print (#76)

--- crates/dkg-cli/run_dkg.sh
+++ crates/dkg-cli/run_dkg.sh
@@ -34,6 +34,13 @@ exit_if_empty "$PRIVATE_KEY" "PRIVATE_KEY"
 exit_if_empty "$DKG_ADDRESS" "DKG_ADDRESS"
 exit_if_empty "$NODE_URL" "NODE_URL"
 
+function exit_if_command_failed() {
+  if [[ ${PIPESTATUS[0]} != 0 ]]; then
+    echo "Error, exiting"
+    exit 1
+  fi
+}
+
 # In order to run this example, your environment must have the following variables set:
 # 1. DKG_ADDRESS:  Address of the DKG contract
 # 2. NODE_URL: Your Celo node
@@ -53,6 +60,7 @@ function get_phase () {
 function do_phase0 () {
   $DKG new --private-key privkey --public-key pubkey
   $CELO dkg:register --node $NODE_URL --address $DKG_ADDRESS --privateKey $PRIVATE_KEY --from $FROM --blsKey pubkey 2>&1 | grep -v libusb
+  exit_if_command_failed
   echo "Registration complete"
   echo 0 > dkg_phase
 }
@@ -61,9 +69,14 @@ function do_phase1 () {
   ### 2. Share Generation (3 parties registed but only 2 appeared!)
 
   $CELO dkg:get --node $NODE_URL --address $DKG_ADDRESS --method group 2>&1 | grep -v libusb > dkg_group
+  $CELO dkg:get --node $NODE_URL --address $DKG_ADDRESS --method participants 2>&1 | grep -v libusb > dkg_group_addresses
+  exit_if_command_failed
+
+  cr=`echo $'\n.'`
+  cr=${cr%.}
 
   while true; do
-      read -p "DKG group details: $(echo "" && cat dkg_group && echo "DKG group hash: `sha256sum dkg_group | awk '{print $1}'`") `echo $'\nDo you approve the DKG group details? > '`" yn
+    read -p "${cr}${cr}${cr}********************************${cr}DKG group details: $(echo "" && cat dkg_group_addresses && echo "DKG group hash: `sha256sum dkg_group_addresses | awk '{print $1}'`") `echo $'\n\nDo you approve the DKG group details? > '`" yn
       case $yn in
           [Yy]* ) break;;
           [Nn]* ) exit 1;;
@@ -73,7 +86,9 @@ function do_phase1 () {
 
 
   $DKG publish-shares --private-key privkey --group ./dkg_group --out-phase phase1_node --output shares
+  exit_if_command_failed
   $CELO dkg:publish --node $NODE_URL --address $DKG_ADDRESS --privateKey $PRIVATE_KEY --from $FROM --data ./shares 2>&1 | grep -v libusb
+  exit_if_command_failed
   echo "Shares published"
 
   echo 1 > dkg_phase
@@ -83,9 +98,12 @@ function do_phase1 () {
 function do_phase2 () {
   ### 3. Response Generation
   $CELO dkg:get --node $NODE_URL --address $DKG_ADDRESS --method shares 2>&1 | grep -v libusb > combined_shares
+  exit_if_command_failed
 
   $DKG publish-responses --in-phase phase1_node --out-phase phase2_node --input ./combined_shares --output responses
+  exit_if_command_failed
   $CELO dkg:publish --node $NODE_URL --address $DKG_ADDRESS --privateKey $PRIVATE_KEY --from $FROM --data ./responses 2>&1 | grep -v libusb
+  exit_if_command_failed
   echo "Responses published"
 
   echo 2 > dkg_phase
@@ -94,6 +112,7 @@ function do_phase2 () {
 
 function do_phase3 () {
   $CELO dkg:get --node $NODE_URL --address $DKG_ADDRESS --method responses 2>&1 | grep -v libusb > combined_responses
+  exit_if_command_failed
 
   ### 4. Since the 3rd participant didn't publish, we have to go to phase 3
 
@@ -105,6 +124,7 @@ function do_phase3 () {
   fi
 
   $CELO dkg:publish --node $NODE_URL --address $DKG_ADDRESS --privateKey $PRIVATE_KEY --from $FROM --data ./justifications 2>&1 | grep -v libusb
+  exit_if_command_failed
   echo "Justifications Published"
   echo 3 > dkg_phase
   #do_phase3
@@ -114,8 +134,10 @@ function do_phase4 () {
   ### 5. Justifications
 
   $CELO dkg:get --node $NODE_URL --address $DKG_ADDRESS --method justifications 2>&1 | grep -v libusb > combined_justifications
+  exit_if_command_failed
 
   $DKG finalize --in-phase phase3_node --input combined_justifications --output result
+  exit_if_command_failed
   echo 4 > dkg_phase
 }
 
