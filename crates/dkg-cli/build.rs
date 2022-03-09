1367e0c770c936936b41c8ccad927988bb914818
Michael Straka <mstraka100@gmail.com>
2022-03-09 15:29:37 -0600

    build.rs works

--- crates/dkg-cli/build.rs
+++ crates/dkg-cli/build.rs
@@ -1,46 +1,31 @@
-use ethers::{contract::Abigen, solc::{Solc, CompilerInput}};
+use ethers::{contract::Abigen};
 use std::{fs::File, io::Write};
 use ethers_solc::{Project, ProjectPathsConfig};
-use serde::{Deserialize, Serialize};
-use serde_json::Result;
 
 const PATH: &str = "../../solidity/";
-const CONTRACT_NAME: &str = "DKG.sol";
+const CONTRACT_NAME: &str = "DKG";
 // Generates the bindings under `src/`
 fn main() {
     // Only re-run the builder script if the contract changes
     println!("cargo:rerun-if-changed={}", PATH);
 
     // compile the DKG contract (requires solc on the builder's system)
-    //let contracts = Solc::new(PATH).build_raw().expect("could not compile");
-    //let contract = contracts.get("DKG").expect("contract not found");
     let project = Project::builder()
         .paths(ProjectPathsConfig::hardhat(PATH).unwrap())
         .build()
         .unwrap();
-    println!("project: {:?}", project);
-    let contract = project.compile().unwrap();
+    let compiler_output = project.compile().unwrap();
+    let contract = compiler_output.find(CONTRACT_NAME).unwrap();
     
-    println!("test: {}", contract.has_compiled_contracts());
-    let mut artifacts = contract.into_artifacts();
-    let artifact = artifacts.next().unwrap().1;
-    let abi = artifact.abi.clone().unwrap();
-    let bytecode = artifact.bytecode.clone().unwrap();
-    //println!("artifact: {:?}", artifact);
-    println!("abi: {:?}", abi);
-    //println!("abi.abi: {:?}", abi.abi);
-    //println!("bytecode: {:?}", bytecode);
-
-    //let abi = contract.abi.clone();
-    let abi_string = serde_json::to_string(&abi).unwrap();
-
     let mut f = File::create("dkg.bin").expect("could not create DKG bytecode file");
-    /*f.write_all(contract.bin.as_bytes())
-        .expect("could not write DKG bytecode to the file");*/
-    //f.write_all(bytecode.as_bytes())
-      //  .expect("could not write DKG bytecode to the file");
+    let bytes = contract.bytecode.clone().unwrap().object.into_bytes().unwrap();
+    f.write_all(&bytes.to_vec())
+        .expect("could not write DKG bytecode to the file");
 
     // generate type-safe bindings to it
+    let abi = contract.abi.as_ref().unwrap();
+    let abi_string = serde_json::to_string(&abi.clone()).unwrap();
+
     let bindings = Abigen::new("DKG", abi_string)
         .expect("could not instantiate Abigen")
         .generate()
