a30471bb1c8b3f5f58f364c25b5167eb1bd72b71
Paul Lange <palango@gmx.de>
2025-06-12 13:09:11 +0200

    android: Increase API version to 24

--- crates/threshold-bls-ffi/cross/create-ndk-standalone.sh
+++ crates/threshold-bls-ffi/cross/create-ndk-standalone.sh
@@ -18,7 +18,7 @@ cd NDK
 
 for ARCH in arm64 arm x86 x86_64; do
     echo "($ARCH)..."
-    "$MAKER" --arch $ARCH --api 21 --install-dir $ARCH
+    "$MAKER" --arch $ARCH --api 24 --install-dir $ARCH
 done
 
 echo 'Updating .cargo/config.toml...'
