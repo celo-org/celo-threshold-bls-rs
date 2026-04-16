df73e61e4819de98661a20bf5f3165ef4285760c
Paul Lange <palango@gmx.de>
2026-04-16 17:22:31 +0200

    refactor: Remove bls-crypto dependency
    Inline the few items actually used from bls-crypto:
    - Blake2s/Blake2x hasher (DirectHasher) as a local blake2_hash() function
    - SIG_DOMAIN constant
    - Buffer FFI struct (moved into threshold-bls-ffi)
    
    Rename ZexeError to BLSError with flattened variants and remove unused
    CurveError enum. Fix empty-slice panic in Buffer::from and pre-existing
    dangerous_implicit_autorefs error in share_ptr.
    
    Drops 37 transitive dependencies (clap, csv, env_logger, lru,
    ark-crypto-primitives, ark-ed-on-bw6-761, etc.) and replaces them with
    a single blake2s_simd dependency.

--- crates/threshold-bls/src/curve/mod.rs
+++ crates/threshold-bls/src/curve/mod.rs
@@ -1,11 +1,2 @@
-/// Wrappers around the BLS12-377 curve from [zexe](https://github.com/scipr-lab/zexe/tree/master/algebra/src/bls12_377)
+/// Wrappers around the BLS12-377 curve
 pub mod bls12377;
-
-use thiserror::Error;
-
-/// Error which unifies all curve specific errors from different libraries
-#[derive(Debug, Error)]
-pub enum CurveError {
-    #[error("Zexe Error: {0}")]
-    BLS12_377(bls12377::ZexeError),
-}
